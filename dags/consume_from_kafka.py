5463613e426215333eba7438229c518734be51f4
adenletchworth <adenrletchworth@gmail.com>
2024-06-14 23:56:02 -0700

    feat: configs for kafka, kafka dag for streaming reddit data

---
+++ dags/consume_from_kafka.py
@@ -0,0 +1,108 @@
+from datetime import datetime, timedelta
+from airflow import DAG # type: ignore
+from airflow.operators.python_operator import PythonOperator # type: ignore
+from airflow.sensors.base import BaseSensorOperator # type: ignore
+from confluent_kafka import Consumer, KafkaError, KafkaException # type: ignore
+from pymongo import MongoClient # type: ignore
+from airflow.utils.decorators import apply_defaults # type: ignore
+import json
+from configs.kafka import bootstrap_servers, topic_name, mongo_uri, mongo_db_name, mongo_collection_name
+
+kafka_config = {
+    'bootstrap.servers': bootstrap_servers,
+    'group.id': 'airflow-consumer-group',
+    'auto.offset.reset': 'earliest'
+}
+
+class KafkaMessageSensor(BaseSensorOperator):
+    @apply_defaults
+    def __init__(self, kafka_config, topic, *args, **kwargs):
+        super(KafkaMessageSensor, self).__init__(*args, **kwargs)
+        self.kafka_config = kafka_config
+        self.topic = topic
+
+    def poke(self, context):
+        consumer = Consumer(self.kafka_config)
+        consumer.subscribe([self.topic])
+
+        msg = consumer.poll(timeout=1.0)
+        if msg is None:
+            consumer.close()
+            return False
+
+        if msg.error():
+            if msg.error().code() == KafkaError._PARTITION_EOF:
+                self.log.info('End of partition reached {}/{}'.format(msg.topic(), msg.partition()))
+                consumer.close()
+                return False
+            elif msg.error():
+                raise KafkaException(msg.error())
+        
+        consumer.close()
+        return True
+
+# Define the consume_and_store_messages function
+def consume_and_store_messages(**kwargs):
+    consumer = Consumer(kafka_config)
+    consumer.subscribe([topic_name])
+
+    client = MongoClient(mongo_uri)
+    db = client[mongo_db_name]
+    collection = db[mongo_collection_name]
+
+    try:
+        while True:
+            msg = consumer.poll(timeout=1.0)
+            if msg is None:
+                break
+
+            if msg.error():
+                if msg.error().code() == KafkaError._PARTITION_EOF:
+                    print('End of partition reached {0}/{1}'.format(msg.topic(), msg.partition()))
+                    continue
+                else:
+                    raise KafkaException(msg.error())
+            else:
+                record_value = msg.value().decode('utf-8')
+                record = json.loads(record_value)
+                collection.insert_one(record)
+                print(f'Record inserted: {record}')
+    except Exception as e:
+        print(f'Error: {e}')
+    finally:
+        consumer.close()
+        client.close()
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime.now() - timedelta(days=1),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+dag = DAG(
+    'kafka_to_mongodb_event_driven',
+    default_args=default_args,
+    description='Consume messages from Kafka and store in MongoDB only when there are messages',
+    schedule_interval=None,
+    catchup=False
+)
+
+# Define the tasks
+kafka_sensor = KafkaMessageSensor(
+    task_id='kafka_message_sensor',
+    kafka_config=kafka_config,
+    topic=topic_name,
+    dag=dag
+)
+
+consume_task = PythonOperator(
+    task_id='consume_and_store_messages',
+    python_callable=consume_and_store_messages,
+    dag=dag
+)
+
+kafka_sensor >> consume_task
--- dags/reddit_to_kafka_dag.py
+++ dags/reddit_to_kafka_dag.py
@@ -2,46 +2,143 @@ from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 import praw
-from kafka import KafkaProducer
+from confluent_kafka import Producer
 import json
-from configs.reddit import client_id, client_secret, user_agent
+import sqlite3
+import os
+from configs.reddit import client_id, client_secret, user_agent, subreddits_list
 from configs.kafka import bootstrap_servers, topic_name
 
-def fetch_reddit_posts(**kwargs):
-    reddit = praw.Reddit(client_id=client_id,
-                         client_secret=client_secret,
-                         user_agent=user_agent)
-    subreddit = reddit.subreddit('programming')
-    
-    posts = []
-    for submission in subreddit.new(limit=100):  
+DB_PATH = '/opt/airflow/dags/db/historical_data_flag.db'
+
+def init_db():
+    if not os.path.exists(DB_PATH):
+        os.makedirs(os.path.dirname(DB_PATH), exist_ok=True)
+        conn = sqlite3.connect(DB_PATH)
+        cursor = conn.cursor()
+        cursor.execute('''CREATE TABLE IF NOT EXISTS fetch_flags (subreddit TEXT PRIMARY KEY, fetched INTEGER)''')
+        conn.commit()
+        conn.close()
+
+def has_historical_data_been_fetched(subreddit_name):
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+    cursor.execute('SELECT fetched FROM fetch_flags WHERE subreddit = ?', (subreddit_name,))
+    result = cursor.fetchone()
+    conn.close()
+    return result is not None and result[0] == 1
+
+def set_historical_data_fetched(subreddit_name):
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+    cursor.execute('INSERT OR REPLACE INTO fetch_flags (subreddit, fetched) VALUES (?, ?)', (subreddit_name, 1))
+    conn.commit()
+    conn.close()
+
+def delivery_report(err, msg):
+    if err is not None:
+        print(f"Delivery failed for record {msg.key()}: {err}")
+    else:
+        print(f"Record {msg.key()} successfully produced to {msg.topic()} [{msg.partition()}] at offset {msg.offset()}")
+
+def fetch_historical_reddit_posts(**kwargs):
+    init_db()
+    reddit = praw.Reddit(client_id=client_id, client_secret=client_secret, user_agent=user_agent)
+    producer_config = {
+        'bootstrap.servers': bootstrap_servers,
+        'message.timeout.ms': 60000  # Increase the message timeout to 60 seconds
+    }
+    producer = Producer(producer_config)
+
+    for subreddit_name in subreddits_list:
+        if has_historical_data_been_fetched(subreddit_name):
+            print(f"Historical data for subreddit {subreddit_name} already fetched. Skipping...")
+            continue
+
+        subreddit = reddit.subreddit(subreddit_name)
+        for submission in subreddit.new(limit=1000):
+            post = {
+                'id': submission.id,
+                'title': submission.title,
+                'selftext': submission.selftext,
+                'created_utc': submission.created_utc,
+                'author': str(submission.author),
+                'subreddit': str(submission.subreddit),
+                'score': submission.score,
+                'ups': submission.ups,
+                'downs': submission.downs,
+                'num_comments': submission.num_comments,
+                'url': submission.url,
+                'permalink': submission.permalink,
+                'is_self': submission.is_self,
+                'over_18': submission.over_18,
+                'spoiler': submission.spoiler,
+                'locked': submission.locked,
+                'stickied': submission.stickied,
+                'edited': submission.edited,
+                'flair_text': submission.link_flair_text,
+                'flair_css_class': submission.link_flair_css_class,
+                'thumbnail': submission.thumbnail,
+                'media': submission.media,
+                'view_count': submission.view_count,
+                'archived': submission.archived,
+                'distinguished': submission.distinguished
+            }
+            producer.produce(topic_name, key=post['id'], value=json.dumps(post), callback=delivery_report)
+
+        set_historical_data_fetched(subreddit_name)
+
+    producer.flush()
+
+def stream_reddit_posts_to_kafka():
+    reddit = praw.Reddit(client_id=client_id, client_secret=client_secret, user_agent=user_agent)
+    producer_config = {
+        'bootstrap.servers': bootstrap_servers,
+        'message.timeout.ms': 60000  # Increase the message timeout to 60 seconds
+    }
+    producer = Producer(producer_config)
+
+    def send_to_kafka(submission):
         post = {
             'id': submission.id,
             'title': submission.title,
             'selftext': submission.selftext,
-            'created_utc': submission.created_utc
+            'created_utc': submission.created_utc,
+            'author': str(submission.author),
+            'subreddit': str(submission.subreddit),
+            'score': submission.score,
+            'ups': submission.ups,
+            'downs': submission.downs,
+            'num_comments': submission.num_comments,
+            'url': submission.url,
+            'permalink': submission.permalink,
+            'is_self': submission.is_self,
+            'over_18': submission.over_18,
+            'spoiler': submission.spoiler,
+            'locked': submission.locked,
+            'stickied': submission.stickied,
+            'edited': submission.edited,
+            'flair_text': submission.link_flair_text,
+            'flair_css_class': submission.link_flair_css_class,
+            'thumbnail': submission.thumbnail,
+            'media': submission.media,
+            'view_count': submission.view_count,
+            'archived': submission.archived,
+            'distinguished': submission.distinguished
         }
-        posts.append(post)
-    
-    return posts
+        producer.produce(topic_name, key=post['id'], value=json.dumps(post), callback=delivery_report)
 
-def stream_to_kafka(**kwargs):
-    ti = kwargs['ti']
-    posts = ti.xcom_pull(task_ids='fetch_reddit_posts')
-    
-    producer = KafkaProducer(bootstrap_servers=bootstrap_servers,
-                             value_serializer=lambda v: json.dumps(v).encode('utf-8'))
-    
-    for post in posts:
-        producer.send(topic_name, post)
-    
+    for subreddit_name in subreddits_list:
+        subreddit = reddit.subreddit(subreddit_name)
+        for submission in subreddit.stream.submissions():
+            send_to_kafka(submission)
+
     producer.flush()
-    producer.close()
 
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2024, 1, 1),
+    'start_date': datetime.now() - timedelta(days=1),
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
@@ -51,22 +148,29 @@ default_args = {
 dag = DAG(
     'reddit_to_kafka',
     default_args=default_args,
-    description='Fetch Reddit posts and stream to Kafka',
+    description='Fetch Reddit posts and stream to Kafka for multiple subreddits',
     schedule_interval=timedelta(hours=1),
+    max_active_runs=1,
 )
 
-fetch_reddit_posts_task = PythonOperator(
-    task_id='fetch_reddit_posts',
-    python_callable=fetch_reddit_posts,
+fetch_historical_task = PythonOperator(
+    task_id='fetch_historical_reddit_posts',
+    python_callable=fetch_historical_reddit_posts,
     provide_context=True,
     dag=dag,
 )
 
-stream_to_kafka_task = PythonOperator(
-    task_id='stream_to_kafka',
-    python_callable=stream_to_kafka,
+stream_reddit_posts_task = PythonOperator(
+    task_id='stream_reddit_posts',
+    python_callable=stream_reddit_posts_to_kafka,
     provide_context=True,
     dag=dag,
+    execution_timeout=timedelta(hours=1),
+    depends_on_past=False,
+    retries=1,
+    retry_delay=timedelta(minutes=5),
+    max_active_tis_per_dag=1,
 )
 
-fetch_reddit_posts_task >> stream_to_kafka_task
+# Task dependency
+fetch_historical_task >> stream_reddit_posts_task
